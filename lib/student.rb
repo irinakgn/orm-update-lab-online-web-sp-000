50600b900052885daf059d20a02ecc955e1e7b07
Irina Kagan <irinagamb@gmail.com>
2019-06-08 17:09:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,6 @@
 require_relative "../config/environment.rb"
 
 class Student
-
   attr_accessor :name, :grade
   attr_reader :id
 
@@ -29,36 +28,42 @@ class Student
          DB[:conn].execute(sql)
     end
 
-    def save
-         sql = <<-SQL
-         INSERT INTO students (name, grade)
-         VALUES (?, ?)
-         SQL
-         DB[:conn].execute(sql, self.name, self.grade)
-
-         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
-    end
 
     def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
     end
-      end
-
-      def save
-        sql = <<-SQL
-        INSERT INTO student (name, grade)
-        VALUES (?, ?)
-        SQL
-
-        DB[:conn].execute(sql, self.name, self.grade)
-
-      end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def self.new_from_db(row)
+  new_student = self.new
+new_student.id = row[0]
+new_student.name = row[1]
+new_student.grade = row[2]
+new_student
+  end
+  def self.new_from_db(row)
+      # create a new Student object given a row from the database
+      student = self.new(row[1], row[2], row[0])
+      student
+    end
+
+    def self.find_by_name(name)
+      # find the student in the database given a name
+      # return a new instance of the Student class
+      sql = <<-SQL
+        SELECT * FROM students
+        WHERE name = ?
+        LIMIT 1
+        SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
 
 
 end
