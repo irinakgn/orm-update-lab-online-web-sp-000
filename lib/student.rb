711e36dbfab96042dcfc8959f006b5bb02529336
Irina Kagan <irinagamb@gmail.com>
2019-06-08 17:34:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ def save
          SQL
          DB[:conn].execute(sql, self.name, self.grade)
 
-         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+         @id = DB[:conn].execute("SELECT * FROM students")[0][0]
 
 
     end
